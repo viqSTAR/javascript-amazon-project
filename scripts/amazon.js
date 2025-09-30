e7307a6623ba66005cb0d29022457ac9fa61efda
viqSTAR <prasadmanthan07@gmail.com>
2025-09-30 15:52:47 +0530

    cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -82,7 +82,14 @@ document.querySelectorAll('.js-add-to-cart')
         });
       }
 
+      let cartQuantity = 0;
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      })
 
-      console.log(cart);
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
+
+
     });
   });
