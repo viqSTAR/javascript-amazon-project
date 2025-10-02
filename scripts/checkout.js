ab64d309732910811140f876784f275ecb455ef0
viqSTAR <prasadmanthan07@gmail.com>
2025-10-02 12:20:09 +0530

    delete procts form html

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -18,7 +18,8 @@ cart.forEach((cartItem) => {
 
   cartSummaryHTMl +=
   `
-    <div class="cart-item-container">
+    <div class="cart-item-container 
+    js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Wednesday, June 15
             </div>
@@ -102,6 +103,10 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
   link.addEventListener('click', () => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
-    console.log(cart);
-  })
-})
+
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    container.remove();
+  });
+});
